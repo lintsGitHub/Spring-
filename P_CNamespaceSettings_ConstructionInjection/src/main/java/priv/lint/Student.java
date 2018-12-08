1a2daf2e63fe5355a3ea88933f2a4727bcac8035
LLS <2145604059@qq.com>
2018-12-08 11:03:26 +0800

    2018/12/08

---
+++ P_CNamespaceSettings_ConstructionInjection/src/main/java/priv/lint/Student.java
@@ -0,0 +1,40 @@
+package priv.lint;
+
+import java.util.List;
+
+public class Student {
+    String name;
+    String stuClass;
+    List hobby;
+
+    public List getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(List hobby) {
+        this.hobby = hobby;
+    }
+
+    public Student(String stuClass) {
+        this.stuClass = stuClass;
+    }
+
+    public Student() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStuClass() {
+        return stuClass;
+    }
+
+    public void setStuClass(String stuClass) {
+        this.stuClass = stuClass;
+    }
+}
---
+++ P_CNamespaceSettings_ConstructionInjection/src/test/java/priv/lint/StudentTest.java
@@ -0,0 +1,12 @@
+package priv.lint;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StudentTest {
+    @Test
+    public void test(){
+
+    }
+}
